f8c8e81a01c57a4d8977f4cd19f2ab9978e2d7dc
alejandro <aleblancogovantes@hotmail.com>
2022-09-24 16:41:19 +0200

    added adept data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,82 @@ import Fungible from './pages/Fungible';
 import React from 'react';
 
 function App() {
+
+  const products = [
+    {
+        id:1,
+        name:"Drape Support",
+        reference: "AM1000",
+        image: "../Imagenes/adept/drape-support.jpg",
+        shortDescription: "Designed to support a sterile drape over the patient, the extremely versatile Drape Support enhances patient set-up and comfort by optimally managing the operational environment.",
+        description: `The Drape Support is crafted entirely from radiolucent, high-performance engineering plastics for superior rigidity, durability and resistance to chemical attack. The Drape Support is lightweight and compact taking up minimal space around the imaging table and even less when folded for storage. With two articulating joints, it can be easily manoeuvred to suit procedural and patient requirements.
+        Set-up is achieved in seconds as the daggerboard slides under the mattress at a convenient position. ` ,
+    },
+    {
+        id: 2,
+        name:"Star System",
+        reference: "AM01600D",
+        image: "../Imagenes/adept/starsystem.jpg",
+        shortDescription: "The STARSystem comprises of the STARBoard, STARSupport and STARTable to form a complete, clinically engineered solution for right side radial access.",
+        description: `Designed to work with any cardiac or radiology procedure table, the STARSystem is quick to install and adjust to variations in patient size. Set-up is achieved in seconds with no consumable components required.` ,
+    },
+    {
+        id:3,
+        name:"Retrograde IR Platform",
+        reference: "AM0420",
+        image: "../Imagenes/adept/retrograde.jpg",
+        shortDescription: "The Retrograde IR Platform provides a stable over patient worksurface, enhancing the operational environment and clinician workflow during retrograde femoral access procedures.",
+        description: `The Retrograde IR Platform offers a stable, radiolucent, height and length appropriate work surface, providing clinical benefits to the current practice of laying procedural equipment on the mattress and patient. It is the ultimate solution for catheter/guide wire manipulation, presenting a large surface area conveniently aligned with the femoral artery site for supporting equipment during a procedure.` ,
+    },
+    {
+        id:4,
+        name:"Antegrade IR Platform",
+        reference: "AM0440",
+        image: "../Imagenes/adept/antegrade.jpg",
+        shortDescription: "The Antegrade IR Platform provides a convenient worksurface to facilitate procedural workflow and enhance the operational environment during antegrade femoral access procedures.",
+        description: `The Antegrade IR Platform provides a convenient worksurface to facilitate procedural workflow and enhance the operational environment during antegrade femoral access procedures. It is the ultimate solution for catheter/guide wire manipulation, presenting a large surface area conveniently aligned with the femoral artery site for supporting equipment during a procedure.` ,
+    },
+    {
+        id:5,
+        name:"Leg support",
+        reference: "AM3000",
+        image: "../Imagenes/adept/lowerleg.jpg",
+        shortDescription: "A complete, clinically engineered solution to assist with Peripheral Artery Disease Interventions.",
+        description: `The Lower Leg Support has been developed to gently immobilise the patient’s leg during Fluoroscopy guided treatment of Critical Limb Ischemia. Clinician led, the ergonomic design optimally positions the leg for procedural requirements desired during lower limb interventions.` ,
+
+    },
+    {
+        id:6,
+        name:"Overhead arm support",
+        reference: "AM40000",
+        image: "../Imagenes/adept/overhead.jpg",
+        shortDescription: "Now MR safe, the Adept Medical Overhead Arm Support is the ideal solution for patient positioning and management during Magnetic Resonance, C-Arm and CT imaging",
+        description: `Designed to comfortably support the supine patient’s arms, it eliminates extreme shoulder flexion, allows abdominal access and removes unwanted artefacts when imaging. This versatile device will support one or both arms in the supine position and a single arm when used with the posterior oblique position.
+        The Overhead Arm Support provides ease of use for the operator and facilitates patient comfort and procedural repeatability.` ,
+
+    },
+    {
+        id:7,
+        name:"ArmSure",
+        reference: "AM0640",
+        image: "../Imagenes/adept/armsure.jpeg",
+        shortDescription: "The ArmSure is particularly useful in the increasing number of procedures performed under conscious sedation where patient restlessness can be an issue. The optional IR Shield provides enhanced clinician safety through additional scatter radiation protection.",
+        description: `The ArmSure prevents a restless patient from compromising the sterile femoral access site while presenting the forearm for IV lines, anaesthesia, etc., throughout the procedure. The ArmSure gently secures both arms of a supine patient in a comfortable adducted position with soft, easy to install, adjustable straps. The unique design allows the straps to be loose while maintaining arm security and patient comfort.` ,
+
+    },
+    {
+        id:8,
+        name:"Prone Support",
+        reference: "AM0700",
+        image: "../Imagenes/adept/prone.jpg",
+        shortDescription: "The ideal solution for supporting and managing a prone patient, it provides comfortable patient positioning during image-guided procedures.",
+        description: `This versatile device will support a patient in the prone position for multiple procedure types. The two-part support provides optimal patient comfort and procedural management, supporting both the arms and face.
+        The arm supporting surface is cantilevered out from the imaging table, accommodating a variety of patient sizes and mobility ranges comfortably. The arms can be positioned according to individual patient shoulder mobility or procedural needs.` ,
+
+    },
+];
+
+
   return (
     <div className="App">
   
@@ -25,7 +101,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/Faq' element={<Faq />} />
         <Route path='/products' element={<Products />} />
-        <Route path='/products/adept' element={<Adept />} />
+        <Route path='/products/adept' element={<Adept products={products} />} />
         <Route path='/products/cardiatis' element={<Cardiatis/>} />
         <Route path='/products/fungible' element={<Fungible />} />
       </Routes>
