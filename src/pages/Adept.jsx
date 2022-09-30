8666838ca002ea2672aa6e3243ddf4dec31accd6
alejandro <aleblancogovantes@hotmail.com>
2022-09-30 17:28:05 +0200

    display list of products with reactStrap cards, might look for a change later, something more original

--- src/pages/Adept.jsx
+++ src/pages/Adept.jsx
@@ -1,12 +1,55 @@
+import { Button, Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
 
-function Adept(){
+function Adept({products}){
 
+
+   
     return (
 
         <>
         <h1>Pagina de adept</h1>
+
+        <p>Dedicated to supporting our customers with meaningful medical solutions to improve people’s lives. 
+            Clinically led solutions for patient & workspace management in Interventional Radiology & Cardiology, 
+            and devices in Otology, Rhinology and Sterilisation, designed and manufactured in New Zealand.</p>
+
+        {products.map( product => {
+            return(
+                <>
+                 <Card className="my-2">
+            <CardImg
+              alt="Card image cap"
+              src={product.image}
+              style={{
+                height: 300,
+              width: 500}}
+              top
+              width="auto"
+            />
+            <CardBody>
+              <CardTitle tag="h5">
+                  {product.name}
+              </CardTitle>
+              <CardText>
+                  {product.shortDescription}
+              </CardText>
+              <CardText>
+                <Button>More details</Button>
+              </CardText>
+            </CardBody>
+          </Card>
+                </>
+            )
+           
+        })}
+
+            
+        
+
+
         </>
+
     )
 }
 
